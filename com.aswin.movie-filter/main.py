68b4823f46dc4d06a58a56c9865f2b4426e754f3
AswinRoy <aswin@dataculture.in>
2016-10-19 01:23:16 +0530

    added comments to main

--- com.aswin.movie-filter/main.py
+++ com.aswin.movie-filter/main.py
@@ -1,13 +1,15 @@
 import pandas as pd
-import constants as const
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+import constants as const
 
 if __name__ == "__main__":
 
     print "Enter the path of the raw data file."
     input_file = raw_input()
 
+    """read the raw file as a dataframe """
     df = pd.read_excel(input_file, header=0).fillna(" ")
     df[const.FEATURES] = df[const.FEATURE_1].map(str) + const.COLON + df[const.FEATURE_2] \
                          + const.COLON + df[const.FEATURE_3] + const.COLON + df[const.FEATURE_4]
@@ -17,10 +19,13 @@ if __name__ == "__main__":
     print "Please enter the number of similar movies to display :"
     similar_number_of_movies = int(raw_input())
 
+    # Calculate TF-IDF of the features
     tfidf_matrix = TfidfVectorizer().fit_transform(df[const.FEATURES])
 
+    # Find the cosine similarity between the records
     result = cosine_similarity(tfidf_matrix[movie_id - 1:movie_id], tfidf_matrix).flatten()
 
+    # Sort the and get indices of the top results
     indices = result.argsort()[:-(similar_number_of_movies + 2):-1]
 
     print "Similar movies and their scores (scale of 0.0 to 1.0)"
